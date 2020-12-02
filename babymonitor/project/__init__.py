1800f435f23e757db5197fee2a8a95e1d91a9b66
adrilene <adrilenefonseca@gmail.com>
2020-12-02 15:32:47 -0300

    deleting database

--- babymonitor/project/__init__.py
+++ babymonitor/project/__init__.py
@@ -2,9 +2,13 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+try:
+    os.remove("appBabyMonitor.db")
+except Exception:
+    pass
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///appBabyMonitor.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
 
--- smartphone/project/__init__.py
+++ smartphone/project/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
+try:
+    os.remove("appSmartphone.db")
+except Exception:
+    pass
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///appSmartphone.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
 
